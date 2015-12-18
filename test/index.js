614e3d2cbaafcfa99e94038926ed2d4e6f7c844e
Ben Cooling <office@bcooling.com.au>
2015-12-18 11:57:45 +1000

    updated

--- test/index.js
+++ test/index.js
@@ -5,8 +5,12 @@ test('dummy test', function (t) {
   t.end();
 });
 
-
 test('dummy test 2', function (t) {
   t.pass('dummy test 2 passed');
+  t.end();
+});
+
+test('dummy test 3', function (t) {
+  t.pass('dummy test 3 passed');
   t.end();
 });
