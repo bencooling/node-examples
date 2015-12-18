4b5204524bcc71f93f1357a260def483d99e9f4e
Ben Cooling <office@bcooling.com.au>
2015-12-18 11:49:43 +1000

    updated

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var PORT = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
 
-    console.log('%d request received', process.pid);
+  console.log('%d request received', process.pid);
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    res.end('Hello world!\n');
+  res.end('Hello world!\n');
 }).listen(PORT);
 
 console.log('%d listening on %d', process.pid, PORT);
